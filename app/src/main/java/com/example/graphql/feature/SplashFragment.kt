36a65d9516542a065bca20161f7902f235e03381
SpirinOleg <Spirin_O_E@mail.ru>
2021-07-07 11:41:42 +0300

    change spash screen on activity

--- app/src/main/java/com/example/graphql/feature/SplashFragment.kt
+++ app/src/main/java/com/example/graphql/feature/SplashFragment.kt
@@ -1,19 +1,23 @@
 package com.example.graphql.feature
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.graphql.BuildConfig
+import com.example.graphql.MainActivity
 import com.example.graphql.R
 import kotlinx.android.synthetic.main.activity_start.*
 import kotlinx.coroutines.*
 import java.text.MessageFormat
 import kotlin.coroutines.CoroutineContext
 
-class SplashFragment: Fragment(R.layout.activity_start), CoroutineScope {
+class SplashFragment: AppCompatActivity(), CoroutineScope {
 
     private lateinit var navController: NavController
 
@@ -24,13 +28,7 @@ class SplashFragment: Fragment(R.layout.activity_start), CoroutineScope {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        navController = findNavController()
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        setContentView(R.layout.activity_start)
 
         splash_version_app.text = MessageFormat.format(
             "{0}{1}",
@@ -41,7 +39,8 @@ class SplashFragment: Fragment(R.layout.activity_start), CoroutineScope {
         launch {
             delay(3000)
             withContext(Dispatchers.Main){
-                navController.navigate(R.id.action_start_activity_to_auth_fragment,null,NavOptions.Builder().setPopUpTo(R.id.start_activity,true).build())
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
             }
         }
     }
